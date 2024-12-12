40fd71469de9a00bdd43ddc6aec7d65b017e40a3
Breno Gabriel <bbbgab123@gmail.com>
2024-12-12 15:20:38 -0300

    FEAT: graph

--- plot/plot.py
+++ plot/plot.py
@@ -3,33 +3,61 @@ from matplotlib.animation import FuncAnimation
 import numpy as np
 from serial import Serial, SerialException
 
+AMOUNT_GRAPH = 100
+target = []
+
+
+def printOnSerial():
+    ser = Serial("COM3", 115200)
+
+    if ser.is_open:
+        print("Conexão estabelecida na porta COM3!")
+
+    try:
+        # Envia um comando pela serial (como string, convertida para bytes)
+        comando = input()  # Substitua pelo comando desejado
+        ser.write(comando.encode("utf-8"))  # Envia o comando para o ESP
+        print(f"Comando enviado: {comando}")
+
+    except KeyboardInterrupt:
+        print("Leitura interrompida.")
+
 
 def updateData(ser: float, y_val: list[float]):
     if ser.in_waiting > 0:
         print("Valor sendo lido")
         line = ser.readline().decode("utf-8").rstrip()
-        new_data = float(line)
+        new_data = float(str(line).split(",")[1])
+        target_read = float(str(line).split(",")[0])
         print(f"Dado lido da serial: {new_data}")
+
+        if len(y_val) > AMOUNT_GRAPH:
+            y_val.pop(0)
+            target.pop(0)
+
         y_val.append(float(new_data))
+        target.append(float(target_read))
 
     x_val = np.arange(0, len(y_val))
-    return x_val, y_val
+
+    return target, x_val, y_val
 
 
 def animate(i: int, ser, y_val):
-    x_val, y_val = updateData(ser, y_val)
+    target, x_val, y_val = updateData(ser, y_val)
 
     plt.cla()
     plt.title("Valores de RPM")
     plt.ylabel("RPM")
     plt.grid()
     plt.tight_layout()
-    plt.plot(x_val, y_val)
+    plt.plot(x_val, y_val, label="Mesured")
+    plt.plot(x_val, target, label="Target")
 
 
 def handlePlot(port: str):
     print(f"Lendo a porta {port}")
-    
+
     if port == "":
         print("Porta não selecionada")
         return {"error": "Porta não selecionada"}
@@ -45,9 +73,9 @@ def handlePlot(port: str):
             interval=50,
             cache_frame_data=False,
         )
-        
+
         plt.show()
-        return None
+        return anim
 
     except SerialException as e:
         return {"error": f"Erro ao acessar a porta serial {port}: {e}"}
@@ -56,3 +84,7 @@ def handlePlot(port: str):
         return {"error": f"Erro ao processar os dados: {ve}"}
     except Exception as e:
         return {"error": f"Ocorreu um erro inesperado: {e}"}
+
+
+printOnSerial()
+handlePlot("COM3")
